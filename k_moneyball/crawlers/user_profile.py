85f986e5c0d6cf04bcf3ad0d93edb712808737e1
MoonSeoJun <msj00130@gmail.com>
2023-10-10 23:02:18 +0900

    Remove unecessary codes

--- k_moneyball/crawlers/user_profile.py
+++ k_moneyball/crawlers/user_profile.py
@@ -17,22 +17,6 @@ class UserProfile:
         return
 
     def get_league_urls(self) -> list[dict]:
-        # league_urls = []
-
-        # req = requests.get(self.root_url, headers=REQUEST_HEADERS)
-
-        # if req.status_code == requests.codes.ok:
-        #     soup = BeautifulSoup(req.text, "lxml")
-
-        #     league_cup_table = soup.find("div", {"class" : "responsive-table"})
-        #     league_cups_tags = league_cup_table.find_all("img", {"class" : "continental-league-emblem"})
-
-        #     for tag in league_cups_tags:
-        #         league = soup.find("a", {"title" : tag['title']})
-        #         league_url = TRANSFER_MARKT_ROOT_URL + league['href']
-
-        #         league_urls.append(league_url)
-
             return self.league_urls
         
     def get_club_urls(self):
@@ -101,7 +85,6 @@ class UserProfile:
 
     def get_player_info(self):
         player_urls = self.get_player_profile_urls()
-        # player_urls = ["https://www.transfermarkt.com/nana-boateng/profil/spieler/236272"]
 
         if player_urls is None:
             return
