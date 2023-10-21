5bbee11e7648cb74320c9594d21c820012debf8e
MoonSeoJun <msj00130@gmail.com>
2023-10-21 18:24:58 +0900

    Modify club profile crawler data save way

--- k_moneyball/crawlers/club_profile_crawler.py
+++ k_moneyball/crawlers/club_profile_crawler.py
@@ -2,31 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 
 import time
-import csv
+import json
 
-from configs import REQUEST_HEADERS, TRANSFER_MARKT_ROOT_URL, CLUB_PROFILE_CSV_COLUMN
+from configs import REQUEST_HEADERS, TRANSFER_MARKT_ROOT_URL
 
 class ClubProfile:
-    def __init__(self) -> None:
+    def __init__(self):
         self.national_num_kr = 87
         self.root_url = f"{TRANSFER_MARKT_ROOT_URL}/wettbewerbe/national/wettbewerbe/87"
         self.league_urls = [
             "https://www.transfermarkt.com/k-league-1/startseite/wettbewerb/RSK1",
             "https://www.transfermarkt.com/k-league-2/startseite/wettbewerb/RSK2"]
-        return
 
-    def get_league_urls(self) -> list[dict]:
+    def get_league_urls(self):
         return self.league_urls
         
-    def get_club_urls(self, is_writing_csv):
-        print(f"=================== is_writing_csv : {is_writing_csv} ===================")
+    def extract_club_info(self):
         league_urls = self.get_league_urls()
-        club_urls = []
-
-        if is_writing_csv:
-            club_csv = open('club5.csv', 'a', newline='')
-            wr = csv.writer(club_csv)
-            wr.writerow(CLUB_PROFILE_CSV_COLUMN)
+        club_info = {
+            "clubs" : []
+        }
 
         for url in league_urls:
             print(url + " Start =====================")
@@ -43,19 +38,17 @@ class ClubProfile:
                     club = tag.a['href']
                     club_url = TRANSFER_MARKT_ROOT_URL + club
 
-                    club_urls.append(club_url)
+                    club_info['clubs'].append(
+                    {
+                        "league" : league_title.string.strip(),
+                        "club_name" : tag.a['title'],
+                        "club_url" : club_url
+                    })
 
-                    print(league_title.string.strip())
                     print(tag.a['title'])
-
-                    if is_writing_csv:
-                        wr.writerow([tag.a['title'], league_title.string.strip()])
 
                 print(url + " Complete =====================")
                 time.sleep(2)
 
-        if is_writing_csv:
-            club_csv.close()
-        
-        print(club_urls)
-        return club_urls
+        with open('./clubs_info.json', 'w') as outfile:
+            json.dump(club_info, outfile, indent='\t')
