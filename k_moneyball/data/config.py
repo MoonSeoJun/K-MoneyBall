92fb68bfd9472f171a331983d2ebd583800061b6
MoonSeoJun <msj00130@gmail.com>
2023-10-22 03:57:55 +0900

    Add connect mongo by altas

---
+++ k_moneyball/data/config.py
@@ -0,0 +1,2 @@
+CLUBS_INFO_JSON_PATH = './clubs_info.json'
+PLAYERS_INFO_JSON_PATH = './players_info.json'
---
+++ k_moneyball/data/init_mongodb.py
@@ -0,0 +1,45 @@
+from pymongo.mongo_client import MongoClient
+
+import json
+
+from config import PLAYERS_INFO_JSON_PATH, CLUBS_INFO_JSON_PATH
+
+def read_collection_all_data(collection):
+    cursor = collection.find()
+
+    while True:
+        try:
+            print(cursor.next())
+        except StopIteration as e:
+            print(e)
+            break
+
+url = "mongodb+srv://msj00130:password@example1.qdwr5rh.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(url)
+
+try:
+    print(client.list_database_names())
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+db = client.get_database('test')
+
+with open(CLUBS_INFO_JSON_PATH, 'r') as club_file:
+    club_data = json.load(club_file)
+
+with open(PLAYERS_INFO_JSON_PATH, 'r') as player_file:
+    player_data = json.load(player_file)
+
+print(db.drop_collection('clubs'))
+print(db.drop_collection('players'))
+
+clubs_colc = db.create_collection('clubs')
+players_colc = db.create_collection('players')
+
+print(clubs_colc.insert_many(club_data['clubs']))
+print(players_colc.insert_many(player_data['players']))
+
+read_collection_all_data(clubs_colc)
+read_collection_all_data(players_colc)
