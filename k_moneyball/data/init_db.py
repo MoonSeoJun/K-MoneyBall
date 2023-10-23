ac33971e4274060dfdc15878748af36008bcb13f
MoonSeoJun <msj00130@gmail.com>
2023-10-23 20:51:03 +0900

    Modify init mongodb

--- k_moneyball/data/init_db.py
+++ k_moneyball/data/init_db.py
@@ -4,16 +4,6 @@ import json
 
 from config import PLAYERS_INFO_JSON_PATH, CLUBS_INFO_JSON_PATH
 
-def read_collection_all_data(collection):
-    cursor = collection.find()
-
-    while True:
-        try:
-            print(cursor.next())
-        except StopIteration as e:
-            print(e)
-            break
-
 url = "mongodb://admin:admin@mongo:27017"
 client = MongoClient(url)
 
@@ -23,7 +13,20 @@ try:
 except Exception as e:
     print(e)
 
-db = client.get_database('football_player_info')
+football_player_info_db = client.get_database('k_moneyball')
+football_player_info_db.create_collection('clubs')
+football_player_info_db.create_collection('players')
+
+
+change_event_info_db = client.get_database('event_sink')
+change_event_info_db.create_collection('club')
+change_event_info_db.create_collection('player')
+
+
+db = client.get_database('k_moneyball')
+
+clubs_colc = db.get_collection('clubs')
+players_colc = db.get_collection('players')
 
 with open(CLUBS_INFO_JSON_PATH, 'r') as club_file:
     club_data = json.load(club_file)
@@ -31,14 +34,5 @@ with open(CLUBS_INFO_JSON_PATH, 'r') as club_file:
 with open(PLAYERS_INFO_JSON_PATH, 'r') as player_file:
     player_data = json.load(player_file)
 
-print(db.drop_collection('clubs'))
-print(db.drop_collection('players'))
-
-clubs_colc = db.create_collection('clubs')
-players_colc = db.create_collection('players')
-
 print(clubs_colc.insert_many(club_data['clubs']))
 print(players_colc.insert_many(player_data['players']))
-
-read_collection_all_data(clubs_colc)
-read_collection_all_data(players_colc)
