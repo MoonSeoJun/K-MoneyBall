e510f0b14d363c2ff5b643170cfe87ad37961af1
MoonSeoJun <msj00130@gmail.com>
2023-12-04 21:57:33 +0900

    Modify to use pymongo instead kafka sink connector.

--- airflow/dags/scrap_transfermarkt_dag.py
+++ airflow/dags/scrap_transfermarkt_dag.py
@@ -1,4 +1,5 @@
 import pymongo
+from pymongo import ReturnDocument
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
@@ -14,6 +15,7 @@ from modules.club import ClubProfileExporter
 from modules.player import PlayerProfileExporter
 
 import logging
+import re
 
 
 def dummy_callable(action):
@@ -67,15 +69,29 @@ with DAG(
 
         if mongo_collection == config.MONGO_COLLECTION_CLUBS:
             for data in datas:
-                result = colc.find_one_and_replace(
-                    {"club_name" : data["club_name"]}, 
-                    data)
+                if colc.find_one({"club_name" : data["club_name"]}):
+                    result = colc.find_one_and_update(
+                        {"club_name" : data["club_name"]}, 
+                        update={"$set" : data},
+                        return_document=ReturnDocument.AFTER)
+                else:
+                    result = colc.insert_one(document=data)
                 logging.info(result)
         else:
             for data in datas:
-                result = colc.find_one_and_replace(
-                    {'Date of birth' : data["Date of birth"], 'shirt_name' : data["shirt_name"]}, 
-                    data)
+                date_of_birth = data["Date of birth/Age"][0:-5]
+                reg_birth = re.compile(f"^{date_of_birth}")
+
+                if colc.find_one({
+                    'Date of birth/Age' : {"$regex" : reg_birth},
+                    'shirt_name' : data["shirt_name"]}):
+                    result = colc.find_one_and_update(
+                        {'Date of birth/Age' : {"$regex" : reg_birth},
+                        'shirt_name' : data["shirt_name"]}, 
+                        update={"$set" : data},
+                        return_document=ReturnDocument.AFTER)
+                else:
+                    result = colc.insert_one(document=data)
                 logging.info(result)
         
         myclient.close()
@@ -86,25 +102,6 @@ with DAG(
     def extract_club_url(url_list):
         club_urls = [club['club_url'] for club in url_list]
         return club_urls
-    
-    @task.branch(task_id="is_need_init_mongo")
-    def is_need_init_mongo(mongo_host, mongo_db, mongo_collection):
-        myclient = pymongo.MongoClient(mongo_host)
-        mydb = myclient.get_database(mongo_db)
-        clubs_colc = mydb.get_collection(mongo_collection)
-
-        is_mongo_empty = clubs_colc.count_documents({}) == 0
-
-        myclient.close()
-
-        if is_mongo_empty == True:
-            return "scrap_club_profile_task"
-        else:
-            return "scrap_club_profile_task_for_verify"
-        
-    is_need_init_mongo_instance = is_need_init_mongo(config.MONGO_HOST, 
-                                                     config.MONGO_DB, 
-                                                     config.MONGO_COLLECTION_CLUBS)
 
     start = PythonOperator(
         task_id="starting_pipeline",
@@ -112,34 +109,10 @@ with DAG(
         op_kwargs={"action": "starting Pipeline"},
         dag=dag
     )
+    
 
     scrap_club_profile_task = ScrapClubProfileOperator.partial(
         task_id="scrap_club_profile_task",
-        http_header=config.REQUEST_HEADERS,
-    ).expand(
-        url=config.KLEAGUE_URLS,
-    )
-
-    export_club_data = export_data(bootstrap_servers=config.BOOTSTRAP_SERVERS,
-                                   topic=config.CLUB_TOPIC,
-                                   data_list=scrap_club_profile_task.output)
-
-    extracted_club_url = extract_club_url(url_list=export_club_data)
-
-    scrap_player_profile_task = ScrapPlayerProfileOperator.partial(
-        task_id="scrap_player_profile_task",
-        http_header=config.REQUEST_HEADERS,
-    ).expand(
-        url=extracted_club_url
-    )
-
-    export_player_data = export_data(bootstrap_servers=config.BOOTSTRAP_SERVERS,
-                                   topic=config.PLAYER_TOPIC,
-                                   data_list=scrap_player_profile_task.output)
-    
-
-    scrap_club_profile_task_for_verify = ScrapClubProfileOperator.partial(
-        task_id="scrap_club_profile_task_for_verify",
         http_header=config.REQUEST_HEADERS,
     ).expand(
         url=config.KLEAGUE_URLS,
@@ -149,21 +122,21 @@ with DAG(
     verify_club_mongo_task = verify_data_by_mongo(mongo_host=config.MONGO_HOST,
                                                   mongo_db=config.MONGO_DB,
                                                   mongo_collection=config.MONGO_COLLECTION_CLUBS,
-                                                  data_list=scrap_club_profile_task_for_verify.output)
+                                                  data_list=scrap_club_profile_task.output)
     
-    extract_club_url_for_verify = extract_club_url(url_list=verify_club_mongo_task)
+    extract_club_url = extract_club_url(url_list=verify_club_mongo_task)
 
-    scrap_player_profile_for_verify_task = ScrapPlayerProfileOperator.partial(
-        task_id="scrap_player_profile_for_verify_task",
+    scrap_player_profile_task = ScrapPlayerProfileOperator.partial(
+        task_id="scrap_player_profile_task",
         http_header=config.REQUEST_HEADERS,
     ).expand(
-        url=extract_club_url_for_verify
+        url=extract_club_url
     )
 
     verify_player_mongo_task = verify_data_by_mongo(mongo_host=config.MONGO_HOST,
                                                   mongo_db=config.MONGO_DB,
                                                   mongo_collection=config.MONGO_COLLECTION_PLAYERS,
-                                                  data_list=scrap_player_profile_for_verify_task.output)
+                                                  data_list=scrap_player_profile_task.output)
 
     finish = PythonOperator(
         task_id="finishing_pipeline",
@@ -172,11 +145,5 @@ with DAG(
         dag=dag
     )
 
-    start >> is_need_init_mongo_instance
-    start >> is_need_init_mongo_instance
-
-    is_need_init_mongo_instance >> scrap_club_profile_task >> export_club_data >> extracted_club_url >> scrap_player_profile_task
-    scrap_player_profile_task >> export_player_data >> finish
-
-    is_need_init_mongo_instance >> scrap_club_profile_task_for_verify >> verify_club_mongo_task >> extract_club_url_for_verify >> scrap_player_profile_for_verify_task
-    scrap_player_profile_for_verify_task >> verify_player_mongo_task >> finish
+    start >> scrap_club_profile_task >> verify_club_mongo_task >> extract_club_url >> scrap_player_profile_task
+    scrap_player_profile_task >> verify_player_mongo_task >> finish
