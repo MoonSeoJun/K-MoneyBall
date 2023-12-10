74342706fdc7af69df633c5f0edc17ee25fdebef
MoonSeoJun <msj00130@gmail.com>
2023-12-10 22:23:05 +0900

    Modify kafka listener to class object

--- kafka_listener/kafka_listener/kafka_listener.py
+++ kafka_listener/kafka_listener/kafka_listener.py
@@ -5,92 +5,95 @@ import time
 import logging
 import json
 
+class KafkaListener:
+    def __init__(self) -> None:
+        bootstrap_servers = ['broker:29092']
+        topic = ['k_moneyball.clubs', 'k_moneyball.players', 'k_moneyball.game_stats']
 
-def create_producer():
-    bootstrap_servers = ['broker:29092']
+        self.producer = self.__create_producer(bootstrap_servers)
+        self.consumer = self.__create_consumer(bootstrap_servers, topic)
 
-    while True:
+    def consume_messages(self):
         try:
-            producer = KafkaProducer(
-                bootstrap_servers=bootstrap_servers,
-                value_serializer=lambda x: json.dumps(x).encode("utf-8")
-            )
+            for msg in self.consumer:
+                decoded_msg = msg.value.decode('utf-8')
+                msg_topic = msg.topic
+                print("============================================")
+                print(f"msg_topic : {msg_topic}")
+                json_decoded_msg = json.loads(decoded_msg)
+                print(f"Received message: {json_decoded_msg}")
 
-            print("Kafka producer connection Complete!!")
+                msg_payload = json.loads(json_decoded_msg['payload'])
+                event_operation_type = msg_payload['operationType']
+                event_document = msg_payload["ns"]["coll"]
+                event_document_id = msg_payload["documentKey"]["_id"]
+                print("============================================")
 
-            return producer
-        except NoBrokersAvailable:
-            print(f"connect kafka error by NoBrokersAvailable")
-            time.sleep(5)
+                event_sink = {
+                    'event_operation_type' : event_operation_type,
+                    'targetDocument' : event_document,
+                    'eventTarget' : event_document_id
+                }
 
-def create_consumer():
-    bootstrap_servers = ['broker:29092']
-    group_id = 'k_moneyball'
-    topic = ['k_moneyball.clubs', 'k_moneyball.players', 'k_moneyball.game_stats']
+                if event_operation_type == "update":
+                    event_description = msg_payload["updateDescription"]
+                    updated_field = event_description['updatedFields']
+                    if len(updated_field) == 1: continue
+                    else: event_sink['updateDescription'] = event_description
+                else:
+                    event_sink['fullDocument'] = msg_payload['fullDocument']
 
-    while True:
-        try:
-            consumer = KafkaConsumer(
-                group_id=group_id,
-                bootstrap_servers=bootstrap_servers,
-                auto_offset_reset='earliest',  # Read messages from the beginning when no offset is stored
-            )
+                self.producer.send(
+                    topic='k_moneyball.sink.event',
+                    value=event_sink
+                )
+                self.producer.flush()
 
-            consumer.subscribe(topics=topic)
+        except KeyboardInterrupt:
+            pass
 
-            print("Kafka consumer connection Complete!!")
+        finally:
+            self.consumer.close()
 
-            return consumer
-        except NoBrokersAvailable:
-            print(f"connect kafka error by NoBrokersAvailable")
-            time.sleep(5)
+    def __create_producer(self, bootstrap_servers):
+        while True:
+            try:
+                producer = KafkaProducer(
+                    bootstrap_servers=bootstrap_servers,
+                    value_serializer=lambda x: json.dumps(x).encode("utf-8")
+                )
 
-def consume_messages(consumer:KafkaConsumer, producer:KafkaProducer):
-    try:
-        for msg in consumer:
-            decoded_msg = msg.value.decode('utf-8')
-            msg_topic = msg.topic
-            print("============================================")
-            print(f"msg_topic : {msg_topic}")
-            json_decoded_msg = json.loads(decoded_msg)
-            print(f"Received message: {json_decoded_msg}")
+                print("Kafka producer connection Complete!!")
 
-            msg_payload = json.loads(json_decoded_msg['payload'])
-            event_operation_type = msg_payload['operationType']
-            event_document = msg_payload["ns"]["coll"]
-            event_document_id = msg_payload["documentKey"]["_id"]
-            print("============================================")
+                return producer
+            except NoBrokersAvailable:
+                print(f"connect kafka error by NoBrokersAvailable")
+                time.sleep(5)
 
-            event_sink = {
-                'event_operation_type' : event_operation_type,
-                'targetDocument' : event_document,
-                'eventTarget' : event_document_id
-            }
+    def __create_consumer(self, bootstrap_servers, topic):
+        group_id = 'k_moneyball'
 
-            if event_operation_type == "update":
-                event_description = msg_payload["updateDescription"]
-                updated_field = event_description['updatedFields']
-                if len(updated_field) == 1: continue
-                else: event_sink['updateDescription'] = event_description
-            else:
-                event_sink['fullDocument'] = msg_payload['fullDocument']
+        while True:
+            try:
+                consumer = KafkaConsumer(
+                    group_id=group_id,
+                    bootstrap_servers=bootstrap_servers,
+                    auto_offset_reset='earliest',  # Read messages from the beginning when no offset is stored
+                )
 
-            producer.send(
-                topic='k_moneyball.sink.event',
-                value=event_sink
-            )
-            producer.flush()
+                consumer.subscribe(topics=topic)
 
-    except KeyboardInterrupt:
-        pass
+                print("Kafka consumer connection Complete!!")
 
-    finally:
-        consumer.close()
+                return consumer
+            except NoBrokersAvailable:
+                print(f"connect kafka error by NoBrokersAvailable")
+                time.sleep(5)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     print("Start Kafka connection")
-    kafka_consumer = create_consumer()
-    kafka_producer = create_producer()
-    consume_messages(kafka_consumer, kafka_producer)
+
+    kafka_listener = KafkaListener()
+    kafka_listener.consume_messages()
